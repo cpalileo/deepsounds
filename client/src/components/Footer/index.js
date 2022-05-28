f211bc27af9d9580f841eeb18c628136909876e5
CPalileo <cpalileo@gmail.com>
2022-05-28 12:26:31 -0700

    initial commit

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">
+        <div className="container">Favorite Playlists</div>
+        <div className="container">Popular Playlists</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
