4fd916d20eb4af01cb600362853a5e17d79eb3ff
Christopher Palileo <cpalileo@gmail.com>
2022-07-25 14:19:53 -0700

    clean vsc src ctrl

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const User = require("./User");
+const Playlists = require("./Playlists");
 
-module.exports = { User, Thought };
+module.exports = { User, Playlists };
--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,32 +1,83 @@
-// const faker = require('faker');
-const userSeeds = require('./userSeed.json');
-const thoughtSeeds = require('./thoughtSeed.json');
-const db = require('../config/connection');
-const { Thought, User } = require('../models');
+const faker = require("faker");
 
-db.once('open', async () => {
-  try {
-    await Thought.deleteMany({});
-    await User.deleteMany({});
+const db = require("../config/connection");
+const { Playlists, User } = require("../models");
 
-    await User.create(userSeeds);
+db.once("open", async () => {
+  await Playlists.deleteMany({});
+  await User.deleteMany({});
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
   }
 
-  console.log('all done!');
+  const createdUsers = await User.collection.insertMany(userData);
+
+  // create friends
+  for (let i = 0; i < 100; i += 1) {
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
+
+    let friendId = userId;
+
+    while (friendId === userId) {
+      const randomUserIndex = Math.floor(
+        Math.random() * createdUsers.ops.length
+      );
+      friendId = createdUsers.ops[randomUserIndex];
+    }
+
+    // update1: check to see if code breaks after deletion
+    await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
+  }
+
+  // create playlists
+  let createdPlaylists = [];
+  for (let i = 0; i < 100; i += 1) {
+    const playlistsName = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+
+    const createdPlaylists = await Playlists.create({
+      playlistsName,
+      username,
+    });
+
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { playlists: createdPlaylists._id } }
+    );
+
+    createdPlaylists.push(createdPlaylists);
+  }
+
+  // create reactions
+  for (let i = 0; i < 100; i += 1) {
+    const reactionBody = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username } = createdUsers.ops[randomUserIndex];
+
+    const randomPlaylistsIndex = Math.floor(
+      Math.random() * createdPlaylists.length
+    );
+    const { _id: playlistsId } = createdPlaylists[randomPlaylistsIndex];
+
+    await Playlists.updateOne(
+      { _id: playlistsId },
+      { $push: { reactions: { reactionBody, username } } },
+      { runValidators: true }
+    );
+  }
+
+  console.log("all done!");
   process.exit(0);
 });
